478be3b2746322ad436b15c8d83c37837d4ece3b
elewint <elewint@gmail.com>
2022-12-06 20:59:34 -0500

    Start implementing levelchecker

--- Been Here Before/Assets/Scripts/Pickup.cs
+++ Been Here Before/Assets/Scripts/Pickup.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
  public Transform holdSpot;  /* Location where penguin will be hold */
-        public LayerMask pickUpMask;
-        public LayerMask slotMask;
         public GameObject successfulMessage;
         public GameObject unsuccessfulMessage;
         public GameObject door;
 
+        private LayerMask pickUpMask;
+        private LayerMask slotMask;
         private GameObject itemHolding;     /* Having private means when we have it pick up, we store it in this variable. When we want to drop it, we can just access this variable again */
+        private LevelChecker lvlChecker;
 
         public Vector3 Direction {get; set; }  /* Pick Up Logic: Need to know which direction the nurse penguin is facing. Get from movement scipt */
 
@@ -19,7 +20,12 @@ public class Pickup : MonoBehaviour
         {
                 pickUpMask = LayerMask.GetMask("Pickup"); /* Layer mask for pick up */
                 slotMask = LayerMask.GetMask("Slot"); /* Layer mask for slot */
+                lvlChecker = GameObject.Find("LevelChecker").GetComponent<LevelChecker>();
         }
+
+        // slots have tags corresponding to the paintings that belong there
+        // get all collisions between smth in pickup layer and slot layer
+        // if all tags are the same, then level is complete!
 
         void Update()
         {
@@ -32,28 +38,26 @@ public class Pickup : MonoBehaviour
                                         
                                         if (slot) {
                                                 // Successfully put on slot
-                                                successfulMessage.SetActive(true);
-                                                unsuccessfulMessage.SetActive(false);
-                                                door.SetActive(true);
+                                                lvlChecker.CheckLevel();
+                                                // successfulMessage.SetActive(true);
+                                                // unsuccessfulMessage.SetActive(false);
+                                                // door.SetActive(true);
                                                 itemHolding.transform.position = slot.transform.position;
-                                                itemHolding.transform.parent = null;
                                                 itemHolding.GetComponent<SpriteRenderer>().sortingOrder = 99; /* Set sorting order to behind player */
-                                                itemHolding = null; /* Set to null because item is no longer being held */
 
                                         } else {
                                                 itemHolding.transform.position = transform.position + Direction;     /* Change the positon of the item so that when the item is drop, it dropped right infront of the player */
-                                                itemHolding.transform.parent = null;   /* Get the item off the player head */
 
                                                 if (itemHolding.GetComponent<Rigidbody2D>()) {
                                                         itemHolding.GetComponent<Rigidbody2D>().simulated = true;  /* Check to make sure that the object stop following the player */
                                                 }
-
-                                                itemHolding = null; /* Set to null because item is no longer being held */
                                         }
 
-                                
+                                        itemHolding.transform.parent = null;   /* Get the item off the player head */
+                                        itemHolding = null; /* Set to null because item is no longer being held */
 
                                 } else {    /* If player is not currently holding an item, pick the item up */
+                                        Debug.Log("should pick up");
 
                                         Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, 1.5f, pickUpMask);   /* pickUpMask will only allow player to get item that can be picked up */
 
